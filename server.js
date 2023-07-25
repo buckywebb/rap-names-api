ec689928998518bdd4b68e0e607c7f80fe90a4d9
buckywebb <bucky_webb@yahoo.com>
2023-07-25 16:11:28 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+//Object
+const rappers = {
+    '21 savage': {
+        'age': 29,
+        'birthName': 'Shéyaa Bin Abraham-Joseph',
+        'birthPlace': 'London, England'
+        },
+    'chance the rapper': {
+        'age': 30,
+        'birthName': 'Chancelor Johnathan Bennett',
+        'birthPlace': 'Chicago, IL'
+    },
+
+    'unknown': {
+        'age': 'unknown',
+        'birthName': 'unknown',
+        'birthPlace': 'unknown'
+    }
+}
+
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:name', (request, response) => {
+    const rapperName = request.params.name.toLowerCase()
+    if(rappers[rapperName]) {
+        response.json(rappers[rapperName].birthName)
+    }else {
+        response.json(rappers.unknown)
+    }
+})
+
+app.listen(PORT, ()=> {
+    console.log(`The server is now running on port ${PORT}.`)
+})
